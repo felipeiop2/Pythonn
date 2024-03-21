63b5fb753f6206eab004a7a35bd64b87357ae8e1
felipeiop2 <felipeiop999@gmail.com>
2024-03-21 18:00:41 -0300

    tipos primitivos

--- Pythonn/ex001.py
+++ Pythonn/ex001.py
@@ -1,1 +1,2 @@
-print('Ola, Mundo teste')
+nome = input('Qual Seu nome ?')
+print('Muito prazer em te conhecer', nome )
---
+++ Pythonn/ex002.py
@@ -0,0 +1,4 @@
+numero1 = int(input('Digite um numeor'))
+numero2 = int(input('Digite outro numero'))
+res = int(numero1) + (numero2)
+print('A soma entre', numero1 ,' e ', numero2, 'Vale', res,)
