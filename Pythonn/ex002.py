143a4886f20caaf170216cbe12bc4c87176bbb10
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-03-22 01:53:00 -0300

    teste notbook

--- Pythonn/ex002.py
+++ Pythonn/ex002.py
@@ -2,3 +2,5 @@ numero1 = int(input('Digite um numeor'))
 numero2 = int(input('Digite outro numero'))
 res = int(numero1) + (numero2)
 print('A soma entre', numero1 ,' e ', numero2, 'Vale', res,)
+
+print('apenas um teste')
