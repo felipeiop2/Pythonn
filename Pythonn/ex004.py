0d8f1a9abf32162e81844eeb022fac776246afa2
felipeiop2 <felipeiop999@gmail.com>
2024-03-25 15:58:05 -0300

    Media de um aluno

---
+++ Pythonn/ex004.py
@@ -0,0 +1,5 @@
+nome = input('Digite o nome do aluno')
+nota = input('Digite uma nota do aluno')
+nota2 = input('Digite a segunda nota')
+media = int(nota) + int(nota2)
+print('A media do aluno {} e de {}'.format(nome, media))
