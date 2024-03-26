1a4cb7f6788043ae69b4e31a3f4ba0b7086a1e7d
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-03-26 14:07:03 -0300

    teste

--- Pythonn/ex002.py
+++ Pythonn/ex002.py
@@ -1,6 +1,7 @@
-numero1 = int(input('Digite um numeor'))
-numero2 = int(input('Digite outro numero'))
-res = int(numero1) + (numero2)
-print('A soma entre', numero1 ,' e ', numero2, 'Vale', res,)
-
-print('apenas um teste')
+#numero1 = int(input('Digite um numeor'))
+#numero2 = int(input('Digite outro numero'))
+#res = int(numero1) + (numero2)
+#print('A soma entre {} e {} equivale a {}'.format(numero1, numero2, res))
+n =(input('Digite Algo'))
+print('{} é esta em letras minusculas ? {}'.format(n,n.islower()))
+print('{} é um numero ? {}'.format(n, n.isnumeric()))
