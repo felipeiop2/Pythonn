1d071b814ffa9e12922fde27df9f8c8a5f7aba5e
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-03-26 20:04:20 -0300

    exercicios

---
+++ Pythonn/ex007.py
@@ -0,0 +1,9 @@
+metros = int(input('Digite um numero em metros'))
+
+print('em Km : {:.3f}'.format(metros/1000))
+print('em HM : {:.2f}'.format(metros/100))
+print('Em DAM : {:.1f}'.format(metros / 10))
+print('Em Metros : {}'.format(metros))
+print('Em DM : {}'.format(metros * 10))
+print('Em CM : {}'.format(metros * 100))
+print('Em MM : {}'.format(metros * 1000))
