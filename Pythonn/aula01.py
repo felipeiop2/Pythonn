bc3e93bac52c3ab64f64a157fd6576a81f3d27ac
felipeiop2 <felipeiop999@gmail.com>
2024-04-02 18:26:26 -0300

    Exercicios

---
+++ Pythonn/aula01.py
@@ -0,0 +1,7 @@
+from math import  sqrt
+num = int(input('Digite um numero'))
+raiz = sqrt(num)
+print('A raiz quadrada de {} e igual a {}'.format(num, raiz))
+import emoji
+
+
---
+++ Pythonn/ex009.py
@@ -0,0 +1,4 @@
+from math import ceil, floor
+num = float(input('Digite um numero real'))
+print('{} arrendondado para cima: {}'.format(num, ceil(num)))
+print('{} Arredondado para baixo : {}'.format(num, floor(num)))
